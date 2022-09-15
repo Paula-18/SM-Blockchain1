a3b602291262a5c2b26084623c899660ea25d4f4
Paula-18 <paunahappy@gmail.com>
2022-09-15 09:06:15 -0700

    validate y test

--- src/blockchain/modules/validate.js
+++ src/blockchain/modules/validate.js
@@ -1,10 +1,11 @@
 import Block from "../block";
 
 export default (blockchain) => {
-    const [genesisBlock, ...blocks] = blockchain
+    const [genesisBlock, ...blocks] = blockchain;
+
     if (JSON.stringify(genesisBlock) !== JSON.stringify(Block.genesis)) throw Error('Bloque Genesis Invalido');
 
-    for (let i = 0; i < blocks.lenght; i+=1) {
+    for (let i = 0; i < blocks.length; i +=1) {
         const {
             previousHash, timestamp, hash, data,
         } = blocks[i];
@@ -14,4 +15,6 @@ export default (blockchain) => {
         if (previousHash !== previousBlock.hash) throw Error('Hash previo invalido o corrupto');
         if (hash !== Block.hash(timestamp, previousHash, data)) throw Error('Hash Invalido');
     }
+
+    return true;
 };
